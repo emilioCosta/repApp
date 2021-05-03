30cd3aada351d104be64fc441dcbea88e3d0e3ff
Emilio Costa <emilio@MacBook-Pro-de-Emilio.local>
2021-05-03 11:24:42 -0300

    Changing to SwiftUI

---
+++ repApp/repAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  repAppApp.swift
+//  repApp
+//
+//  Created by Emilio Costa on 03/05/21.
+//
+
+import SwiftUI
+
+@main
+struct repAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
